70b26514ee4bdd18565ae4aeac98c2f5f33e7ee3
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-21 17:54:42 -0600

    Documents Empty State

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const Documents = () => {
-  return <div>This is a protected page: /documents</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center space-y-4">
+      <Image
+        src={"/empty.png"}
+        height={"300"}
+        width={"300"}
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src={"/empty-dark.png"}
+        height={"300"}
+        width={"300"}
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="mr-2 size-4" />
+        Create a Note
+      </Button>
+    </div>
+  );
 };
 
 export default Documents;
