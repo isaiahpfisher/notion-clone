702cf11d09d6af1627e329439fa299752384097b
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-22 17:17:17 -0600

    Cover Image Upload, Remove, Replace

---
+++ components/Cover.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/useCoverImage";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
+
+interface CoverProps {
+  url?: string;
+  preview?: boolean;
+}
+
+export const Cover = ({ url, preview }: CoverProps) => {
+  const coverImage = useCoverImage();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+  const params = useParams();
+  const { edgestore } = useEdgeStore();
+
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      });
+    }
+
+    removeCoverImage({ id: params.documentId as Id<"documents"> });
+  };
+
+  return (
+    <div
+      className={cn(
+        "group relative h-[35vh] w-full",
+        !url && "h-[12vh]",
+        url && "bg-muted",
+      )}
+    >
+      {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
+      {url && !preview && (
+        <div className="absolute bottom-5 right-5 flex items-center gap-x-2 opacity-0 hover:opacity-100">
+          <Button
+            onClick={() => coverImage.onReplace(url)}
+            className="text-xs text-muted-foreground"
+            variant={"outline"}
+            size={"sm"}
+          >
+            <ImageIcon className="mr-2 size-4" />
+            Change Cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            className="text-xs text-muted-foreground"
+            variant={"outline"}
+            size={"sm"}
+          >
+            <X className="mr-2 size-4" />
+            Remove
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
