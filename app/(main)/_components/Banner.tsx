51c56b59dd4b734f50432caf023441a2dde839fa
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-22 14:30:29 -0600

    Archived Page Banner

---
+++ app/(main)/_components/Banner.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import ConfirmModal from "@/components/modals/ConfirmModal";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+interface BannerProps {
+  documentId: Id<"documents">;
+}
+
+export const Banner = ({ documentId }: BannerProps) => {
+  const router = useRouter();
+  const remove = useMutation(api.documents.remove);
+  const restore = useMutation(api.documents.restore);
+
+  const onRemove = () => {
+    const promise = remove({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Deleting Note",
+      success: "Note Deleted",
+      error: "Failed to Delete Note",
+    });
+
+    router.push("/documents");
+  };
+
+  const onRestore = () => {
+    const promise = restore({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Restoring Note",
+      success: "Note Restored",
+      error: "Failed to Restore Note",
+    });
+  };
+
+  return (
+    <div className="flex w-full items-center justify-center gap-x-2 bg-rose-500 p-2 text-center text-sm text-white">
+      <p>This page is in the Trash.</p>
+      <Button
+        size={"sm"}
+        onClick={onRestore}
+        variant={"outline"}
+        className="border-white bg-transparent p-1 px-2 font-normal text-white hover:bg-primary/5 hover:text-white"
+      >
+        Restore Page
+      </Button>
+      <ConfirmModal onConfirm={onRemove}>
+        <Button
+          size={"sm"}
+          onClick={onRemove}
+          variant={"outline"}
+          className="border-white bg-transparent p-1 px-2 font-normal text-white hover:bg-primary/5 hover:text-white"
+        >
+          Delete Forever
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./Title";
+import { Banner } from "./Banner";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -42,6 +43,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
           <Title initialData={document} />
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
